9878dae3eb37d4174adda74b5ce783093e6c2f4b
Alumme <student@Alumme.local>
2016-01-20 10:46:12 -0600

    initial commit

---
+++ collegeProfile/CollegeClass.swift
@@ -0,0 +1,13 @@
+//
+//  CollegeClass.swift
+//  collegeProfile
+//
+//  Created by Alumme on 1/20/16.
+//  Copyright © 2016 Alumme. All rights reserved.
+//
+
+import UIKit
+
+class CollegeClass: NSObject {
+
+}
---
+++ collegeProfile/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  collegeProfile
+//
+//  Created by Alumme on 1/20/16.
+//  Copyright © 2016 Alumme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    @IBOutlet weak var myTableView: UITableView!
+    
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+
+
+}
+
